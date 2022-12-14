aa110fc9876b7221cc5b76be06657de59c4d26be
shagunsharma6677 <shagunsharma6677@gmail.com>
2022-12-14 20:05:40 +0530

    day-2 commit

--- lowly-nest-999/src/App.js
+++ lowly-nest-999/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar/Navbar"
+import Footer from "./Components/Footer/Footer.jsx"
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+      <br/>
+      <Login/>
+      <br/>
+      <Footer/>
     </div>
   );
 }
